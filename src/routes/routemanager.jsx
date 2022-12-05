31e2935951142daafdef79591818c58f82b79306
TF8088 <tiagofarinha03@outlook.com>
2022-12-05 11:12:24 +0000

    location service test

--- src/routes/routemanager.jsx
+++ src/routes/routemanager.jsx
@@ -1,32 +1,32 @@
-import React , { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Start } from '../screens/start/start';
 import { Home } from '../screens/Home/home';
  
 
 export default function RouteManager() {
-    
+
     const [firststart, setfirststart] = useState(Boolean);
-    
-    useEffect(() => {        
+
+    useEffect(() => {
         AsyncStorage.getItem('@firststart').then(value => {
             if (value === null) /* Never was Opened */  {
                 AsyncStorage.setItem('@firststart', value = 'true'); 
                 setfirststart(true);
             } else /* Was Opened */ {
-                setfirststart(true);
+                setfirststart(false);
             }
         })
     }, [])
 
-    /* Print the status */
+    /* Print the status*/
     console.log(firststart);
 
     if (firststart == true) {
         /* Start -> First load */
         return Start();
     } else {
-        /* Load Data -> Show Home Page */
-        return Home();
+        /* Load Home Page */
+        return Start();
     }
 }
--- src/screens/start/start.jsx
+++ src/screens/start/start.jsx
@@ -1,9 +1,13 @@
-import { View, Text, Image } from "react-native";
-import React, { useEffect, useState } from "react";
 import style from "./start-style";
+import { View, Text, Image } from "react-native";
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from "react";
+import { getCords } from "../../components/location";
 
 export function Start() {
-    
+    getCords();
+
+
     return (
         <View style={style.container}>
             <Image source={require('../../assets/weatherlogo.png')} style={style.logo} />
